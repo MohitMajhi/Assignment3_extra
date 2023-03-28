af693a3330292955c4d91f261f1bc40c36f1463a
Mohit <mohitmajhi99@gmail.com>
2023-03-28 17:17:40 +0530

    added files

---
+++ src/main/java/com/app/Assignment3ExtraApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3ExtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3ExtraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/Assignment3ExtraApplicationTests.java
@@ -0,0 +1,24 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Assignment3ExtraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void case1()
+	{
+		assertEquals(0, 0);
+	}
+	@Test
+	void case2()
+	{
+		assertEquals(0, 0);
+	}
+}
